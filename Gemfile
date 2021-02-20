61d5185880b7637c923bef85963f8499c9a6bc25
gebageba <nagaikanae34@gmail.com>
2021-02-20 08:58:09 +0800

    rspec

--- Gemfile
+++ Gemfile
@@ -30,6 +30,9 @@ gem 'bootsnap', '>= 1.4.4', require: false
 
 group :development, :test do
   gem 'pry-rails'
+  gem 'rspec-rails'
+  gem 'factory_bot_rails'
+  gem 'faker'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
---
+++ spec/factories/records.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :record do
+   date                 { Faker::Date.in_date_period }
+   memo                 { Faker::Lorem.sentence }  
+   subject_id           { Faker::Number.within(range: 0..18) }
+   price                { Faker::Number.number(digits: 5) }
+   detail               { Faker::Lorem.sentence }
+   user                 { FactoryBot.create(:user) }
+  end
+end 

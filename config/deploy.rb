c3b269e7384f76f2754aecd7f5b57858d0b96068
gebageba <nagaikanae34@gmail.com>
2021-02-27 07:25:56 +0800

    set unicorn.rake

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,10 @@ lock "~> 3.15.0"
 
 set :application, "record-app"
 set :repo_url, "git@github.com:gebageba/record-app.git"
+
+# ブランチを指定する
+set :branch, "main"
+
 # バージョンが変わっても共通で参照するディレクトリを指定。
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 

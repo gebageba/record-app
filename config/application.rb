1fa431cac916046b2eb86f790d08d73f46ab096b
gebageba <nagaikanae34@gmail.com>
2021-02-28 11:04:26 +0800

    for health check add host

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module RecordApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.hosts << ".record-for-kakutei.ml"
   end
 end

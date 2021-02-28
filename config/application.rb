8132feec39e523a77909959bb66aadeecaa072ef
gebageba <nagaikanae34@gmail.com>
2021-02-28 12:13:31 +0800

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,6 @@ module RecordApp
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.hosts << ".record-for-kakutei.ml"
+    config.hosts << "ap-northeast-1.elb.amazonaws.com"
   end
 end
